54d4845c2940987d7c346efa51a144adafbdde56
Raphael Picard <raphaelpicard@outlook.fr>
2021-09-08 15:54:54 +0200

    feat: Add Hook for fetchData

---
+++ src/utils/useFetch.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react';
+
+const useFetch = (url, number) => {
+    const [response, setResponse] = useState([]);
+    const [error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+
+    const fetchData = () => {
+      return new Promise(async(resolve, reject) => {
+        let page = 1;
+        let data = [];
+        while (data.length < number) {
+          const res = await fetch(`${url}${page}`);
+          const json = await res.json();
+          if (res.status >= 400) {
+            return reject(json.status_message) 
+          } 
+          data = data.concat(json.results);
+          page +=1
+        }
+        resolve(data);
+      })
+    };
+
+    useEffect(() => {
+        if (!url) return;
+        fetchData()
+          .then(data => setResponse(data.slice(0, number)))
+          .catch(m => setError(m))
+          .finally(() => setIsLoading(false))
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+  
+    return { response, error, isLoading };
+};
+export default useFetch;
