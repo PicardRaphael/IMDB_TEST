363b720fc165e5bbc3f93b9e4a3d5cecfd6a0722
Raphael Picard <raphaelpicard@outlook.fr>
2021-09-08 13:16:13 +0200

    refactor: Clear preview

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,14 +1,5 @@
 import StoryRouter from 'storybook-react-router';
 
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-}
 export const decorators = [
   StoryRouter(),
   (Story) => (
