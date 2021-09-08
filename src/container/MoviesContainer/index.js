3714ed53259eef8713076667afdb7dbda98db198
Raphael Picard <raphaelpicard@outlook.fr>
2021-09-08 16:15:54 +0200

    feat: Custom CircularProgress

--- src/container/MoviesContainer/index.js
+++ src/container/MoviesContainer/index.js
@@ -1,4 +1,19 @@
+import api from '../../config/api';
+import useFetch from '../../utils/useFetch';
+import MyCircularProgress from '../../styles/CircularProgressStyle';
+
 const  MoviesContainer = () => {
+  const {isLoading, response, error} = useFetch(api.url_movies, 50)
+  
+  if (!isLoading) {
+    return <MyCircularProgress size={60} />
+  }
+
+  if (error) {
+    console.log(error);
+    return <MyCircularProgress size={60} />
+  }
+
   return (
     <div>
         MoviesContainer
