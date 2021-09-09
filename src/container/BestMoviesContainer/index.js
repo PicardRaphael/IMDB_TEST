e2b8ed3b52e51c6860f3203b3345730b89a66af7
Raphael Picard <raphaelpicard@outlook.fr>
2021-09-09 09:27:22 +0200

    feat: Add BestMoviesContainer

--- src/container/BestMoviesContainer/index.js
+++ src/container/BestMoviesContainer/index.js
@@ -1,10 +1,23 @@
+import api from '../../config/api';
+import useFetch from '../../utils/useFetch';
+import MyCircularProgress from '../../styles/CircularProgressStyle';
+import Movies from '../../component/Movies';
+
 const  BestMoviesContainer = () => {
-    return (
-      <div className="App">
-          <h1>BestMoviesContainer</h1>
-      </div>
-    );
+  const {isLoading, response, error} = useFetch(api.url_top_rated, 10)
+  if (isLoading) {
+    return <MyCircularProgress size={60} />
+  }
+
+  if (error) {
+    console.log(error);
+    return <MyCircularProgress size={60} />
   }
   
-  export default BestMoviesContainer;
+  return (
+    <Movies  movies={response} />
+  );
+}
+  
+export default BestMoviesContainer;
   
